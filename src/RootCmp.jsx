99e838946cd566ca9eba4a99701c44247b1727f6
Gal1997 <Galisraeli97@gmail.com>
2025-01-07 14:01:58 +0200

    First commit Initialized RootCmp

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,28 +1,32 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from "react-router-dom";
+import "./styles/mainStyles.scss";
+import { UserMsg } from "./cmps/Usermsg";
+import { Login } from "./pages/Login";
+import { HomePage } from "./pages/HomePage";
+import { SignUp } from "./pages/SignUp";
+import { MondayIndex } from "./pages/MondayIndex";
+import SideBar from "./cmps/SideBar";
+import { BoardDetails } from "./pages/BoardDetails";
+import Navbar from "./cmps/NavBar";
 
-import './styles/mainStyles.scss'
-import { UserMsg } from './cmps/Usermsg'
-import { Login } from './pages/Login'
-import { HomePage } from './pages/HomePage'
-import { SignUp } from './pages/SignUp'
-import { MondayIndex } from './pages/MondayIndex'
 function RootCmp() {
-
+  const location = useLocation();
+  const showSidebarAndNavBar = location.pathname.startsWith("/workspace");
   return (
-    <div >
-      <Router>
+    <div>
+      {showSidebarAndNavBar && <SideBar />}
+      {showSidebarAndNavBar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="workspace/board/:boardId" element={<BoardDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/index" element={<MondayIndex />} />
+      </Routes>
 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/index" element={<MondayIndex />} />
-
-        </Routes>
-      </Router>
       <UserMsg />
     </div>
-  )
+  );
 }
 
-export default RootCmp
+export default RootCmp;
