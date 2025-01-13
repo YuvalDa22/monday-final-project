acefe533deac0d2085a3152d4c54e24057ea39e7
Gal1997 <Galisraeli97@gmail.com>
2025-01-13 18:22:10 +0200

    * Initialized TaskDetails outlet

--- src/cmps/group/TaskList.jsx
+++ src/cmps/group/TaskList.jsx
@@ -1,12 +1,13 @@
-import Input from "@mui/joy/Input";
-import { useState } from "react";
-import { updateBoard } from "../../store/board/board.actions"
-import { TaskPreview } from "../task/TaskPreview";
-import { utilService } from "../../services/util.service";
+import Input from '@mui/joy/Input'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { updateBoard } from '../../store/board/board.actions'
+import { TaskPreview } from '../task/TaskPreview'
+import { utilService } from '../../services/util.service'
 
 export function TaskList({ board, group, cmpTitles, cmpsOrder }) {
   const [editingTaskId, setEditingTaskId] = useState(null)
-  const [tempTitle, setTempTitle] = useState("")
+  const [tempTitle, setTempTitle] = useState('')
 
   const handleEdit = (taskId, currentTitle) => {
     setEditingTaskId(taskId)
@@ -15,33 +16,34 @@ export function TaskList({ board, group, cmpTitles, cmpsOrder }) {
 
   //save updated task name
   const handleSave = (taskId) => {
-    if(tempTitle.trim() && tempTitle !== ""){ //TODO - check functionality
-      updateBoard(board, group.id, taskId, {key: "title", value: tempTitle})
+    if (tempTitle.trim() && tempTitle !== '') {
+      //TODO - check functionality
+      updateBoard(board, group.id, taskId, { key: 'title', value: tempTitle })
     }
-    handleCancel();
+    handleCancel()
   }
 
   const handleCancel = () => {
     setEditingTaskId(null)
-    setTempTitle("")
+    setTempTitle('')
   }
 
   const onAddItem = () => {
-    const newTask = { id: utilService.makeId(), title: tempTitle };
-    const updatedTasks = [...group.tasks, newTask];
+    const newTask = { id: utilService.makeId(), title: tempTitle }
+    const updatedTasks = [...group.tasks, newTask]
 
-    updateBoard(board, group.id, null, { key: "tasks", value: updatedTasks });
-    setTempTitle(""); 
-  };
+    updateBoard(board, group.id, null, { key: 'tasks', value: updatedTasks })
+    setTempTitle('')
+  }
 
   return (
     <div>
-      <table className="custom-table">
+      <table className='custom-table'>
         <thead>
           <tr>
-            <td className="empty-cell"></td>
+            <td className='empty-cell'></td>
             {cmpTitles.map((title, index) => (
-              <td key={index} className="header-cell">
+              <td key={index} className='header-cell'>
                 {title}
               </td>
             ))}
@@ -50,62 +52,64 @@ export function TaskList({ board, group, cmpTitles, cmpsOrder }) {
         <tbody>
           {group.tasks.map((task) => (
             <tr key={task._id}>
-              <td className="task-cell">
-
-              {editingTaskId === task.id ? (
-                <Input
-                type="text"
-                value={tempTitle}
-                onChange={(event) => setTempTitle(event.target.value)}
-                onKeyDown={(event) => {
-                  if(event.key === "Enter") handleSave(task.id)
-                  if(event.key === "Escape") handleCancel()
-                }}
-              />
-              ) : (
-                <span onClick ={()=> handleEdit(task.id, task.title)}>
-                  {task.title}
-                </span>
-              )}
+              <td className='task-cell'>
+                {editingTaskId === task.id ? (
+                  <Input
+                    type='text'
+                    value={tempTitle}
+                    onChange={(event) => setTempTitle(event.target.value)}
+                    onKeyDown={(event) => {
+                      if (event.key === 'Enter') handleSave(task.id)
+                      if (event.key === 'Escape') handleCancel()
+                    }}
+                  />
+                ) : (
+                  <span onClick={() => handleEdit(task.id, task.title)}>
+                    <Link to={`task/${task.id}`}>{task.title}</Link>
+                  </span>
+                )}
               </td>
-              <TaskPreview 
-              group={group}
-              board={board} 
-              task={task} 
-              cmpsOrder={cmpsOrder} />
+              <TaskPreview
+                group={group}
+                board={board}
+                task={task}
+                cmpsOrder={cmpsOrder}
+              />
             </tr>
           ))}
-         <tr>
-            <td colSpan={cmpTitles.length + 1} className="add-item-row">
+          <tr>
+            <td colSpan={cmpTitles.length + 1} className='add-item-row'>
               <td>
-              <Input
-                
-                type="text"
-                placeholder="+ Add item"
-                value={tempTitle}
-                onChange={(event) => setTempTitle(event.target.value)}
-                onKeyDown={(event) => {
-                  if (event.key === "Enter") onAddItem();
-                  if (event.key === "Escape") handleCancel();
-                }}
-                sx={{
-                  border: "none", 
-                  outline: "none", 
-                 background: "transparent", 
-                }}
-              />
+                <Input
+                  type='text'
+                  placeholder='+ Add item'
+                  value={tempTitle}
+                  onChange={(event) => setTempTitle(event.target.value)}
+                  onKeyDown={(event) => {
+                    if (event.key === 'Enter') onAddItem()
+                    if (event.key === 'Escape') handleCancel()
+                  }}
+                  sx={{
+                    border: 'none',
+                    outline: 'none',
+                    background: 'transparent',
+                  }}
+                />
               </td>
             </td>
           </tr>
         </tbody>
       </table>
     </div>
-  );
+  )
 }
 
-
-					{/* last tr - group summary */}
-          {/* TODO - IMPLEMENT SUMMARY */}
-					{/* <GroupSummary group={group} /> */}
-
-
+{
+  /* last tr - group summary */
+}
+{
+  /* TODO - IMPLEMENT SUMMARY */
+}
+{
+  /* <GroupSummary group={group} /> */
+}
--- src/pages/BoardDetails.jsx
+++ src/pages/BoardDetails.jsx
@@ -1,78 +1,76 @@
-import { useSelector } from "react-redux"
-import { BoardHeader } from "../cmps/board/BoardHeader"
-import { GroupPreview } from "../cmps/group/GroupPreview"
-import { useEffect } from "react"
-import { loadBoards } from "../store/board/board.actions"
-import { showErrorMsg } from "../services/event-bus.service"
-import { Button } from "@mui/material";
-import { updateBoard } from "../store/board/board.actions"
-import { utilService } from "../services/util.service"
-
-
+import { useSelector } from 'react-redux'
+import { BoardHeader } from '../cmps/board/BoardHeader'
+import { GroupPreview } from '../cmps/group/GroupPreview'
+import { useEffect } from 'react'
+import { loadBoards } from '../store/board/board.actions'
+import { showErrorMsg } from '../services/event-bus.service'
+import { Button } from '@mui/material'
+import { updateBoard } from '../store/board/board.actions'
+import { Outlet, useParams } from 'react-router-dom'
+import { utilService } from '../services/util.service'
 
 export function BoardDetails() {
-	const allBoards = useSelector((storeState) => storeState.boardModule.boards)
+  const allBoards = useSelector((storeState) => storeState.boardModule.boards)
+  const { boardId } = useParams()
 
-	useEffect(() => {
-		onLoadBoards()
-	},[])
+  useEffect(() => {
+    onLoadBoards()
+  }, [])
 
-	async function onLoadBoards() {
-		try {
-			await loadBoards();
-		} catch (error) {
-			showErrorMsg("Cannot load boards");
-			console.error(error);
-		}
-	}
+  async function onLoadBoards() {
+    try {
+      await loadBoards()
+    } catch (error) {
+      showErrorMsg('Cannot load boards')
+      console.error(error)
+    }
+  }
 
-	if (!allBoards || allBoards.length === 0) return <div>Loading...</div>
-	
-	const onAddGroup = () => { 
-		const board = allBoards[0]
-		console.log("board",board)
-		if(!board) return;
-		const newGroup = {
-			id: utilService.makeId(),
-			title: "New Group",
-			tasks: [],
-			style: {},
-		}
-		// console.log("New group",newGroup)
-		const updatedGroups = [...board.groups, newGroup]
-		// console.log("updated groups",updatedGroups)
-		updateBoard(board, null, null, {key: "groups", value: updatedGroups})
-	}
+  if (!allBoards || allBoards.length === 0) return <div>Loading...</div>
 
-	return (
-		<>
-			<div className='main-container'>
-				<BoardHeader board={allBoards[0]} />
-				{allBoards[0].groups &&
-					allBoards[0].groups.map((group) => (
-						<GroupPreview
-							board={allBoards[0]}
-							group={group}
-							cmpTitles={allBoards[0].cmpTitles}
-							cmpsOrder = {allBoards[0].cmpsOrder}
-							key={group.id}
-						/>
-					))}
-				<Button
-				variant="contained"
-				color="primary"
-				onClick={onAddGroup}
-				>Add new group
-					</Button>
-			</div>
-		</>
-	)
+  const onAddGroup = () => {
+    const board = allBoards[0]
+    console.log('board', board)
+    if (!board) return
+    const newGroup = {
+      id: utilService.makeId(),
+      title: 'New Group',
+      tasks: [],
+      style: {},
+    }
+    // console.log("New group",newGroup)
+    const updatedGroups = [...board.groups, newGroup]
+    // console.log("updated groups",updatedGroups)
+    updateBoard(board, null, null, { key: 'groups', value: updatedGroups })
+  }
 
-		// Ofir & Yuval
-		// <>
-		// <BoardHeader board={board}/>
-		// {groups && groups.map(group => <GroupPreview group={group} board={board} key={group._id}/>)}
+  return (
+    <>
+      <div className='main-container'>
+        <BoardHeader board={allBoards[0]} />
+        {allBoards[0].groups &&
+          allBoards[0].groups.map((group) => (
+            <GroupPreview
+              board={allBoards[0]}
+              group={group}
+              cmpTitles={allBoards[0].cmpTitles}
+              cmpsOrder={allBoards[0].cmpsOrder}
+              key={group.id}
+            />
+          ))}
+        <Button variant='contained' color='primary' onClick={onAddGroup}>
+          Add new group
+        </Button>
+      </div>
+      <Outlet context={boardId} /> {/* Task Details ! */}
+    </>
+  )
 
-		// <button>add group</button>
-		// </>
+  // Ofir & Yuval
+  // <>
+  // <BoardHeader board={board}/>
+  // {groups && groups.map(group => <GroupPreview group={group} board={board} key={group._id}/>)}
+
+  // <button>add group</button>
+  // </>
 }
