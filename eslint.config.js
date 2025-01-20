037f022b1905530a917d4db592812df5709151d7
ofirgady <OFirgady@gmail.com>
2025-01-20 12:41:04 +0200

    chakra provided v1

--- eslint.config.js
+++ eslint.config.js
@@ -4,37 +4,43 @@ import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
+
 export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+	{ ignores: ['dist'] },
+	{
+		files: ['**/*.{js,jsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				ecmaFeatures: { jsx: true },
+				sourceType: 'module',
+			},
+		},
+    compilerOptions: {
+      module: 'ESNext',
+      moduleResolution: 'Bundler',
+      skipLibCheck: true,
+      paths: {
+        '@/*': ['./src/*'],
       },
     },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      'no-unused-vars': 'off',
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'react/prop-types': 'off', // Disable the annoying missing props validation in our project!!!!
-    },
-  },
+		settings: { react: { version: '18.3' } },
+		plugins: {
+			react,
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			'no-unused-vars': 'off',
+			...react.configs.recommended.rules,
+			...react.configs['jsx-runtime'].rules,
+			...reactHooks.configs.recommended.rules,
+			'react/jsx-no-target-blank': 'off',
+			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+			'react/prop-types': 'off', // Disable the annoying missing props validation in our project!!!!
+		},
+	},
 ]
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import RootCmp from './RootCmp.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
 import { HashRouter as Router } from 'react-router-dom'
+import { ProviderChakra } from './cmps/ui/provider-chakra.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <Router>
-      <RootCmp />
-    </Router>
-  </Provider>
+	<Provider store={store}>
+		<ProviderChakra>
+			<Router>
+				<RootCmp />
+			</Router>
+		</ProviderChakra>
+	</Provider>
 )
