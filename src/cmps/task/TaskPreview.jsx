e93f6d1bf447b909ca0e1d8b5cdf74e3a8afabb2
Gal Israeli <galisraeli97@gmail.com>
2025-01-29 12:23:17 +0200

    * tiny fix

--- src/cmps/task/TaskPreview.jsx
+++ src/cmps/task/TaskPreview.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux'
 import { DynamicCmp } from './DynamicCmp'
 import { boardService } from '../../services/board.service'
 import { useEffect } from 'react'
-import { updateBoard } from '../../store/board/board.actions'
+import { updateBoard, logActivity } from '../../store/board/board.actions'
 
 export function TaskPreview({ task, cmpsOrder, board, group }) {
   return (
@@ -19,16 +19,6 @@ export function TaskPreview({ task, cmpsOrder, board, group }) {
                 message: `${cmp.charAt(0).toUpperCase() + cmp.slice(1)}`,
                 free_txt: `Changed to ${data.title}`,
               })
-              // board.activities.unshift(
-              //   boardService.createActivityLog(
-              //     board._id,
-              //     group.id,
-              //     task.id,
-              //     `${cmp.charAt(0).toUpperCase() + cmp.slice(1)}`, // make the first letter uppercase
-              //     `Changed to ${data.title}`,
-              //     null
-              //   )
-              // )
               updateBoard(group.id, task.id, { key: cmp, value: data.id })
             }}
           />
