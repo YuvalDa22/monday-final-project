7e5343c952df3472390b80d2a534b978d4629b0a
Yuval Dadon <yuvidadon@gmail.com>
2025-03-06 14:32:38 +0200

    add new board button in sidebar

--- src/cmps/layout/SideBar.jsx
+++ src/cmps/layout/SideBar.jsx
@@ -1,12 +1,21 @@
 // Sidebar.jsx
 //import React from "react";
-import { Divider } from '@mui/material'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
-import { getSvg } from '../../services/util.service'
-import { StarBorderOutlined } from '@mui/icons-material'
-import { boardService } from '../../services/board.service'
+import { Divider } from '@mui/material';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { getSvg } from '../../services/util.service';
+import { StarBorderOutlined } from '@mui/icons-material';
+import { boardService } from '../../services/board.service';
+import {
+  Button,
+  ButtonGroup,
+  IconButton as MuiIconButton,
+  Stack as MuiStack,
+} from '@mui/material';
 
-const SvgIcon = ({ iconName, options = { height: '17', width: '17', color: 'currentColor' } }) => {
+const SvgIcon = ({
+  iconName,
+  options = { height: '17', width: '17', color: 'currentColor' },
+}) => {
   return (
     <i
       className={`${iconName}`} // to fix annoying icons not being in the center
@@ -15,33 +24,39 @@ const SvgIcon = ({ iconName, options = { height: '17', width: '17', color: 'curr
         pointerEvents: 'none',
       }}
     ></i>
-  ) // So clicking directly on the SVG won't create an ugly black background
-}
+  ); // So clicking directly on the SVG won't create an ugly black background
+};
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import { DropdownMenu } from 'radix-ui';
 
 export default function Sidebar() {
-  const navigate = useNavigate()
-  const location = useLocation() // Get current route
-  const match = location.pathname.match(/\/workspace\/board\/([^/]+)/) // since SideBar isn't inside <Route> in RootCmp we can't use useParams
-  const boardId = match ? match[1] : null
+  const navigate = useNavigate();
+  const location = useLocation(); // Get current route
+  const match = location.pathname.match(/\/workspace\/board\/([^/]+)/); // since SideBar isn't inside <Route> in RootCmp we can't use useParams
+  const boardId = match ? match[1] : null;
 
-  const [allBoardsTitle, setAllBoardsTitle] = useState([])
+  const [allBoardsTitle, setAllBoardsTitle] = useState([]);
 
   useEffect(() => {
-    fetchBoardsTitle()
-  }, [])
+    fetchBoardsTitle();
+  }, []);
   const fetchBoardsTitle = async () => {
-    const titles = await boardService.getAllBoardsTitle()
-    setAllBoardsTitle(titles)
-  }
+    const titles = await boardService.getAllBoardsTitle();
+    setAllBoardsTitle(titles);
+  };
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       {/* Navigation Links */}
-      <ul className='sidebar-links'>
-        <Link to='/workspace' style={{ all: 'unset' }}>
-          <li className={`sidebar-item ${location.pathname === '/workspace' ? 'active' : ''}`}>
-            <SvgIcon iconName={'sidebar_home'} /> <span className='textInSidebar'>Home</span>
+      <ul className="sidebar-links">
+        <Link to="/workspace" style={{ all: 'unset' }}>
+          <li
+            className={`sidebar-item ${
+              location.pathname === '/workspace' ? 'active' : ''
+            }`}
+          >
+            <SvgIcon iconName={'sidebar_home'} />{' '}
+            <span className="textInSidebar">Home</span>
           </li>
         </Link>
 
@@ -50,51 +65,84 @@ export default function Sidebar() {
             location.pathname.includes('/workspace/myWork') ? 'active' : ''
           }`}
         >
-          <SvgIcon iconName={'sidebar_myWork'} /> <span className='textInSidebar'>My work</span>
+          <SvgIcon iconName={'sidebar_myWork'} />{' '}
+          <span className="textInSidebar">My work</span>
         </li>
       </ul>
-      <Divider className='divider' />
-      <ul className='sidebar-links'>
-        <li className='sidebar-item'>
-          <SvgIcon iconName='sidebar_favorites' />
-          <span className='textInSidebar' style={{ position: 'relative', right: 2 }}>
+      <Divider className="divider" />
+      <ul className="sidebar-links">
+        <li className="sidebar-item">
+          <SvgIcon iconName="sidebar_favorites" />
+          <span
+            className="textInSidebar"
+            style={{ position: 'relative', right: 2 }}
+          >
             Favorites
           </span>
         </li>
       </ul>
-      <Divider className='divider' />
-      <ul className='sidebar-links'>
+      <Divider className="divider" />
+      <ul className="sidebar-links">
         <li
           className={`sidebar-item ${
             location.pathname.includes('/workspace/workspaces') ? 'active' : ''
           }`}
         >
           <SvgIcon iconName={'sidebar_workspaces'} />
-          <span className='textInSidebar'>Workspaces</span>
+          <span className="textInSidebar">Workspaces</span>
         </li>
       </ul>
       {/* Workspace Section */}
-      <div className='workspace-section'>
-        <ul className='workspace-links'>
+      <div className="workspace-section">
+        <ul className="workspace-links">
           {/*obj looks like this {id: XXX , title: YYY} */}
           {allBoardsTitle.map((obj) => {
             return (
               <div
                 key={obj.id}
                 onClick={() => {
-                  navigate(`/workspace/board/${obj.id}`)
-                  window.location.reload() // Forces a refresh
+                  navigate(`/workspace/board/${obj.id}`);
+                  window.location.reload(); // Forces a refresh
                 }}
               >
-                <li className={`workspace-item ${boardId === obj.id ? 'active' : ''}`}>
+                <li
+                  className={`workspace-item ${
+                    boardId === obj.id ? 'active' : ''
+                  }`}
+                >
                   <SvgIcon iconName={'sidebar_workspace_projectIcon'} />
-                  <span className='textInSidebar'>{obj.title}</span>
+                  <span className="textInSidebar">{obj.title}</span>
                 </li>
               </div>
-            )
+            );
           })}
         </ul>
       </div>
+      <Divider className="divider" />
+      <ul className="sidebar-links">
+        <li className="sidebar-item">
+          <ButtonGroup variant="contained" className="new-board-buttons">
+            <Button
+              onClick={() => handleAddBoard(true)}
+              className="add-board-button"
+            >
+              Add New Board
+            </Button>
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger asChild>
+                <Button className="dropdown-button">
+                  <SvgIcon
+                    className="add-item"
+                    iconName="sidebar_add_item"
+                    options={{ height: 22, width: 22, color: '#ffff',
+                    position: 'fixed'}}
+                  />
+                </Button>
+              </DropdownMenu.Trigger>
+            </DropdownMenu.Root>
+          </ButtonGroup>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
