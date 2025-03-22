8b318ca655e12816c94baeb4b8e0b91dbc9965ca
Yuval Dadon <yuvidadon@gmail.com>
2025-03-22 18:01:38 +0200

    Update home page layout and styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,18 @@
-import { AppHeader } from "../cmps/layout/AppHeader";
-import { useNavigate } from "react-router-dom";
+import { AppHeader } from '../cmps/layout/AppHeader';
+import { useNavigate } from 'react-router-dom';
 export function HomePage() {
   const navigate = useNavigate();
 
   return (
     <div className="home-page">
       <AppHeader />
+      <div className="hero-img"></div>
       <div className="hero-layout">
         <div className="hero">
           <h1>A platform for company tasks managment</h1>
         </div>
         <div>
-          <button
-            className="hero-btn"
-            onClick={() => {
-              navigate("/login");
-            }}
-          >
+          <button className="hero-btn" onClick={() => navigate('/login')}>
             Get Started
           </button>
         </div>
