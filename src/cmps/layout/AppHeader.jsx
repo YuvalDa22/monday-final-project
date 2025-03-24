48aae4e59fe84650b4d74f05b481d213501116b4
Gal Israeli <galisraeli97@gmail.com>
2025-03-24 16:02:39 +0200

    homepage ui adjustments

--- src/cmps/layout/AppHeader.jsx
+++ src/cmps/layout/AppHeader.jsx
@@ -1,23 +1,23 @@
-import { useNavigate } from "react-router";
+import { useNavigate } from 'react-router'
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 export function AppHeader() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   return (
-    <div className="header-flex">
+    <div className='header-flex'>
       <div>
         <img
-          onClick={() => navigate("/")}
-					src='https://res.cloudinary.com/ofirgady/image/upload/v1742651944/nkafdc4fmxgpx3ysvx3r.png'
-          alt="logo"
+          onClick={() => navigate('/')}
+          src='https://res.cloudinary.com/ofirgady/image/upload/v1742651944/nkafdc4fmxgpx3ysvx3r.png'
+          alt='logo'
         />
         <h2>Tomorrow</h2>
       </div>
       <div>
-        <button onClick={() => navigate("/login")}>Log in</button>
+        <button onClick={() => navigate('/login')}>Log in</button>
         {/* <button>Start Demo</button> */}
       </div>
     </div>
-  );
+  )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,29 @@
-import { AppHeader } from '../cmps/layout/AppHeader';
-import { useNavigate } from 'react-router-dom';
+import { AppHeader } from '../cmps/layout/AppHeader'
+import { useNavigate } from 'react-router-dom'
 export function HomePage() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   return (
-    <div className="home-page">
+    <div className='home-page'>
       <AppHeader />
-      <div className="hero-img"></div>
-      <div className="hero-layout">
-        <div className="hero">
+
+      <div className='hero-layout'>
+        <div className='hero'>
           <h1>A platform for company tasks managment</h1>
         </div>
         <div>
-          <button className="hero-btn" onClick={() => navigate('/login')}>
+          <button className='hero-btn' onClick={() => navigate('/login')}>
             Get Started
           </button>
         </div>
       </div>
+      <div className='hero-img-container'>
+        <img
+          className='hero-img'
+          src='https://res.cloudinary.com/dv5xfztht/image/upload/v1742657854/Screenshot_2025-03-22_at_17.37.04_ff5vcd.png'
+          alt='Hero'
+        />
+      </div>
     </div>
-  );
+  )
 }
